7d405b9779fb38087eb7dc71017b05577e9e6460
Sawasdee7123 <marc@mallolas.com>
2025-08-11 15:03:22 +0800

    Unindex Widget

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,7 @@ module.exports = {
   siteUrl: 'https://calculadoraciclosdesueno.com',
   generateRobotsTxt: true, // Generate robots.txt
   sitemapSize: 5000,
+  exclude: [
+    '/widget' // This path will not appear in the sitemap
+  ],
 };
